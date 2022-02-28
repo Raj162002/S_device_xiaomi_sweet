3fca5310d162502cad707cafa8a143a8c7afb8d1
Doctor Strange <faizanbashir380@gmail.com>
2022-02-28 05:47:50 +0000

    sweet: minor changes & switch to proton clang & vantom kernel

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -5,10 +5,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common AOSP configurations
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-PRODUCT_NAME := arrow_sweet
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -16,8 +16,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-DEVICE_MAINTAINER := daniml3
-TARGET_INCLUDE_PIXEL_CHARGER := true
+# Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
