e8c9b73bccca2b65ee0c7c21a6a5e1bf324ffcd8
Rajeevan <81076257+Raj162002@users.noreply.github.com>
2022-04-02 21:00:11 +0530

    telephony things for lineage
    change from common.mk to common full phone.mk

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit common lineageOS configurations
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
